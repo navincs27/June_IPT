833d0871162c81d5d5bda756066c7259d8370c38
premkumarmunusamy <prem@66044@gmail.com>
2024-06-22 23:27:36 +0530

    add signin loginpage

--- src/main/java/com/Luma/pages/LoginPage.java
+++ src/main/java/com/Luma/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.Luma.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.Luma.base.BaseClass;
@@ -12,4 +14,15 @@ public class LoginPage extends BaseClass{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	
+	@FindBy(id = "send2")
+	private WebElement signIN;
+	
+	public static void Loginpage() {
+		
+		clickOnElement(driver, signIN);
+	}
+	
+	
 }

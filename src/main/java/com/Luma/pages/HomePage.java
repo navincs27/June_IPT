f25ce536c7f229493b23c096e773d628d65b9ac9
Danusreek07 <danusreerishikumar@gmail.com>
2024-06-22 22:07:25 +0530

    Added xpath for mens

--- src/main/java/com/Luma/pages/HomePage.java
+++ src/main/java/com/Luma/pages/HomePage.java
@@ -1,6 +1,8 @@
 package com.Luma.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.Luma.base.BaseClass;
@@ -8,8 +10,25 @@ import com.Luma.base.BaseClass;
 public class HomePage extends BaseClass{
 
 	WebDriver driver;
+	
+	@FindBy(xpath ="//a[@id='ui-id-5']")
+	private WebElement men;
+	
+	@FindBy(xpath ="//a[@id='ui-id-17']")
+	private WebElement tops;
+	
+	@FindBy(xpath ="//a[@id='ui-id-19']")
+	private WebElement jackets;
+	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	public void categoryPage() {
+		mouseover(men, driver);
+		mouseover(tops, driver);
+		clickTheElement(jackets, driver);
+	}
+	
 }

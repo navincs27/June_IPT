be4629af47e91b44f482deff32c3e3af3e8ecb27
Vignesh <vigneshramasamy9710@gmail.com>
2024-06-23 08:22:13 +0530

    Applied Discount

--- src/main/java/com/Luma/pages/PaymentPage.java
+++ src/main/java/com/Luma/pages/PaymentPage.java
@@ -1,6 +1,8 @@
 package com.Luma.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.Luma.base.BaseClass;
@@ -11,5 +13,8 @@ public class PaymentPage extends BaseClass{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(id="block-discount-heading")
+	private WebElement apply_Discount;
 
 }
